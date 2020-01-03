4d8f6b3493e6ec05c257bb213abdd4683606239b
niedziol <michalekniedziolka@gmail.com>
2020-01-03 16:14:47 +0100

    Threadpool join failed

--- future.c
+++ future.c
@@ -1,14 +1,40 @@
+#include <stdio.h>
 #include "future.h"
 
 typedef void *(*function_t)(void *);
 
+static void fun(void* arg, size_t size) {
+    future_t* f = arg;
+    f->result = f->callable.function(f->callable.arg, f->callable.argsz, &f->result_size);
+    sem_post(&f->finished);
+}
+
 int async(thread_pool_t *pool, future_t *future, callable_t callable) {
+    future->callable = callable;
+    int err = sem_init(&future->finished, 0, 0);
+    if (err != 0) {
+        fprintf(stderr, "ERROR: sem_init failed\n");
+        return -1;
+    }
+
+    runnable_t r;
+    r.function = fun;
+    r.arg = future;
+    r.argsz = sizeof(future_t);
+
+    defer(pool, r);
+
+    return 0;
+}
+
+int map(thread_pool_t* pool, future_t* future, future_t* from,
+        void *(*function)(void *, size_t, size_t*)) {
   return 0;
 }
 
-int map(thread_pool_t *pool, future_t *future, future_t *from,
-        void *(*function)(void *, size_t, size_t *)) {
-  return 0;
-}
+void* await(future_t* future) {
+    sem_wait(&future->finished);
+    sem_destroy(&future->finished);
 
-void *await(future_t *future) { return 0; }
+    return future->result;
+}
--- future.h
+++ future.h
@@ -10,7 +10,10 @@ typedef struct callable {
 } callable_t;
 
 typedef struct future {
-
+    callable_t callable;
+    sem_t finished;
+    void* result;
+    size_t result_size;
 } future_t;
 
 int async(thread_pool_t *pool, future_t *future, callable_t callable);
